644485cbd732f11b11c78277f8866deca532d6fe
kuangcheng <kuangc1987@gmail.com>
2016-01-21 00:40:06 +0800

    XHR test

---
+++ somejavascript/04XHR.js
@@ -0,0 +1,20 @@
+
+
+
+
+var url = "http://p4.qhimg.com/dmfd/208_110_100/t017893fcfee277f6f3.jpg";
+//var url = "http://news.gamedog.cn/a/20151026/1425057.html";
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+	console.log(xhr.readyState);
+	if (xhr.readyState == 4) {
+		if( (xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+			alert(xhr.responseText);
+		} else {
+			alert('Request was unsuccessfual:' + xhr.status);
+		}
+	}
+
+}
+xhr.open('get', url, true);
+xhr.send(null);
