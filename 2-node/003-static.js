254086001e794c2e14fb99d57dd8c10a94dbf158
kuangcheng <kuangcheng-iri@360.cn>
2019-06-20 22:25:10 +0800

    静态网站优化

--- 2-node/003-static.js
+++ 2-node/003-static.js
@@ -7,15 +7,22 @@ http.createServer(function(req, res) {
 	var pathname = url.parse(req.url).pathname;
 	var query = url.parse(req.url).query;
 	
+	
 	if (pathname == '/' && query == null) {
-		pathname = "testform.html";
+		pathname = "index.html";
 	}
 	
+	console.log(pathname);
 	
 	
-	var extname = path.extname(pathname);
 	
-	fs.readFile("./static/" + pathname, function(err, data) {
+	var pathr = "./static/webdemo/" + path.normalize(pathname);
+	
+	
+	// 后缀名
+	var extname = path.extname(pathname);	
+	
+	fs.readFile(pathr, function(err, data) {
 		if (err) {
 			fs.readFile("./static/404.html", function (err, data) {
 				res.writeHead(404, {"Content-Type" : "text/html;charset=utf8"});
@@ -24,24 +31,24 @@ http.createServer(function(req, res) {
 			return;
 		}
 		
-		var mineType = getMime(extname);
-		res.writeHead(200, {"Content-Type" : mineType})
-		res.end(data);
+		getMime(extname, function(mime) {
+			res.writeHead(200, {"Content-Type" : mime})
+			res.end(data);		
+		});
+		
 	});
 	
 	
 }).listen(3000);
 
 
-function getMime(extname) {
-	switch(extname) {
-		case ".html":
-			return "text/html";
-		case ".css":
-			return "text/css";
-		case ".js":
-			return "text/javascript";
-		case ".jpg":
-			return "image/jpg";
-	}
+function getMime(extname, callback) {
+	fs.readFile("./file/mime.json", function(err, data){
+		if (err) {
+			throw Error("can't find mime.json file!");
+		}
+		var mimeJSON = JSON.parse(data);
+		var mime = mimeJSON[extname] || "text/plain";
+		callback( mime );
+	});
 }
